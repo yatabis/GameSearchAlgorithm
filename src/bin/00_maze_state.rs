9e65e6c463ea4e1137f7b9a64a458a7518e54fa0
yatabis <39yatabis.rim8820xxx@gmail.com>
2023-07-16 22:59:15 +0900

    add 00_maze_state

---
+++ src/bin/00_maze_state.rs
@@ -0,0 +1,110 @@
+use std::fmt::{Debug, Formatter};
+use rand::rngs::SmallRng;
+use rand::{RngCore, SeedableRng};
+
+const H: usize = 3;
+const W: usize = 4;
+const END_TURN: i32 = 4;
+
+struct Coord {
+    x: usize,
+    y: usize,
+}
+
+struct MazeState {
+    points: [[i32; W]; H],
+    turn: i32,
+    character: Coord,
+    game_score: i32,
+}
+
+#[allow(non_upper_case_globals)]
+impl MazeState {
+    const dx: [i32; 4] = [1, -1, 0, 0];
+    const dy: [i32; 4] = [0, 0, 1, -1];
+
+    fn new(seed: u64) -> Self {
+        let mut rng_for_construct = SmallRng::seed_from_u64(seed);
+        let y = rng_for_construct.next_u32() as usize % H;
+        let x = rng_for_construct.next_u32() as usize % W;
+        let character = Coord { x, y };
+        let mut points = [[0; W]; H];
+        for y in 0..H {
+            for x in 0..W {
+                if y == character.y && x == character.x { continue; }
+                points[y][x] = rng_for_construct.next_u32() as i32 % 10;
+            }
+        }
+        Self {
+            points,
+            turn: 0,
+            character,
+            game_score: 0
+        }
+    }
+
+    fn is_done(&self) -> bool {
+        self.turn == END_TURN
+    }
+
+    fn advance(&mut self, action: usize) {
+        self.character.x = (self.character.x as i32 + Self::dx[action]) as usize;
+        self.character.y = (self.character.y as i32 + Self::dy[action]) as usize;
+        if self.points[self.character.y][self.character.x] > 0 {
+            self.game_score += self.points[self.character.y][self.character.x];
+            self.points[self.character.y][self.character.x] = 0;
+        }
+        self.turn += 1;
+    }
+
+    fn legal_actions(&self) -> Vec<usize> {
+        let mut actions = Vec::new();
+        for action in 0..4 {
+            let ty = self.character.y as i32 + Self::dy[action];
+            let tx = self.character.x as i32 + Self::dx[action];
+            if ty >= 0 && ty < H as i32 && tx >= 0 && tx < W as i32 {
+                actions.push(action);
+            }
+        }
+        actions
+    }
+}
+
+impl Debug for MazeState {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let s = (0..H).map(|h| {
+            (0..W).map(|w| {
+                if self.character.y == h && self.character.x == w {
+                    "@"
+                } else if self.points[h][w] > 0 {
+                    ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"][self.points[h][w] as usize]
+                } else {
+                    "."
+                }
+            }).collect::<Vec<_>>().join("")
+        }).collect::<Vec<_>>().join("\n");
+        writeln!(f, "turn:\t{}\nscore:\t{}\n{}", self.turn, self.game_score, s)
+    }
+}
+
+type State = MazeState;
+
+fn random_action(state: &State, rng: &mut SmallRng) -> usize {
+    let legal_actions = state.legal_actions();
+    legal_actions[rng.next_u32() as usize % legal_actions.len()]
+}
+
+fn play_game(seed: u64) {
+    // let mut rng = SmallRng::seed_from_u64(0);
+    let mut rng = SmallRng::from_entropy();
+    let mut state = State::new(seed);
+    println!("{:?}", state);
+    while !state.is_done() {
+        state.advance(random_action(&state, &mut rng));
+        println!("{:?}", state);
+    }
+}
+
+fn main() {
+    play_game(121321);
+}
